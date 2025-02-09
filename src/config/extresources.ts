fa82df84aafbef0344d334871a505168c6127e4c
nrf24l01 <s.nevrovsky@ya.ru>
2025-02-09 23:00:35 +0300

    Config for navbar

---
+++ src/config/extresources.ts
@@ -0,0 +1,9 @@
+interface ExtResources {
+  name: string
+  link: string
+}
+
+export const ext_resources: ExtResources[] = [
+  { name: "pasta", link: "pasta.example.com" },
+  {name: "storage", link: "storage.example.com"},
+];
