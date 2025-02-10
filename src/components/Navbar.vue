cc17ffe5f5ab9874b49f07e69331212969961fad
stenli <s.nevrovsky@ya.ru>
2025-02-10 09:14:28 +0300

    Created configurable main menu and fixing styles

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -1,28 +1,30 @@
 <script setup>
-import { ext_resources } from '@/config/extresources';
+import { ext_resources, in_resources } from '@/config/extresources';
 </script>
 
 <template>
   <div class="flex h-screen bg-gray-900 text-white">
     <!-- Sidebar -->
     <div class="hidden lg:flex flex-col w-64 bg-gray-800 p-4 relief_bg">
-      <a href="https://codingprojects.ru" class="flex items-center space-x-2 text-lg font-semibold">
+      <a href="https://codingprojects.ru" class="flex items-center space-x-2 text-lg font-semibold mb-12">
         <img src="https://codingprojects.ru/images/icons/icons8-idea-64.png" alt="Logo" class="h-8">
         <span>GeekClass</span>
       </a>
       <nav class="mt-4">
-        <ul class="space-y-2">
-          <li><a href="https://codingprojects.ru/insider/courses" class="block py-2 px-3 rounded hover:bg-gray-700">Мои курсы</a></li>
-          <li><a href="https://codingprojects.ru/insider/market" class="block py-2 px-3 rounded hover:bg-gray-700">Магазин</a></li>
+        <ul class="space-y-2" v-for="resource in in_resources">
+          <li><a :href="resource.link" 
+              class="block py-2 px-3 rounded hover:bg-gray-700">{{ resource.name }}</a></li>
         </ul>
       </nav>
-      <div class="mt-auto">
+      <hr class="border border-white/10 mb-6 mt-6">
+      <div class="lg:flex flex-col w-64 p-4 text-white/50">
+        <span class="text-small">Ресурсы</span>
         <ul v-for="resource in ext_resources">
           <a :href="resource.link" target="_blank" class="block text-sm hover:text-gray-400">{{ resource.name }}</a>
         </ul>
       </div>
     </div>
-    
+
     <!-- Mobile Menu -->
     <div class="lg:hidden w-full">
       <Disclosure as="nav" class="bg-gray-800 shadow-md relief_bg_mobile">
@@ -32,15 +34,18 @@ import { ext_resources } from '@/config/extresources';
               <img src="https://codingprojects.ru/images/icons/icons8-idea-64.png" alt="Logo" class="h-8">
               <span>GeekClass</span>
             </a>
-            <DisclosureButton class="inline-flex items-center justify-center p-2 rounded-md text-gray-400 hover:bg-gray-700 focus:outline-none">
+            <DisclosureButton
+              class="inline-flex items-center justify-center p-2 rounded-md text-gray-400 hover:bg-gray-700 focus:outline-none">
               <Bars3Icon class="h-6 w-6" aria-hidden="true" />
             </DisclosureButton>
           </div>
         </div>
         <DisclosurePanel class="lg:hidden bg-gray-900 p-2">
           <nav class="space-y-2">
-            <a href="https://codingprojects.ru/insider/courses" class="block py-2 px-3 rounded hover:bg-gray-700">Мои курсы</a>
-            <a href="https://codingprojects.ru/insider/market" class="block py-2 px-3 rounded hover:bg-gray-700">Магазин</a>
+            <a href="https://codingprojects.ru/insider/courses" class="block py-2 px-3 rounded hover:bg-gray-700">Мои
+              курсы</a>
+            <a href="https://codingprojects.ru/insider/market"
+              class="block py-2 px-3 rounded hover:bg-gray-700">Магазин</a>
           </nav>
         </DisclosurePanel>
       </Disclosure>
@@ -66,8 +71,8 @@ export default {
 .relief_bg_mobile {
   background-image: url("data:image/svg+xml,%3Csvg xmlns='http://www.w3.org/2000/svg' width='351' height='292.5' viewBox='0 0 1080 900'%3E%3Cg fill-opacity='0.03'%3E%3Cpolygon fill='%23444' points='90 150 0 300 180 300'/%3E%3Cpolygon points='90 150 180 0 0 0'/%3E%3Cpolygon fill='%23AAA' points='270 150 360 0 180 0'/%3E%3Cpolygon fill='%23DDD' points='450 150 360 300 540 300'/%3E%3Cpolygon fill='%23999' points='450 150 540 0 360 0'/%3E%3Cpolygon points='630 150 540 300 720 300'/%3E%3Cpolygon fill='%23DDD' points='630 150 720 0 540 0'/%3E%3Cpolygon fill='%23444' points='810 150 720 300 900 300'/%3E%3Cpolygon fill='%23FFF' points='810 150 900 0 720 0'/%3E%3Cpolygon fill='%23DDD' points='990 150 900 300 1080 300'/%3E%3Cpolygon fill='%23444' points='990 150 1080 0 900 0'/%3E%3C/g%3E%3C/svg%3E");
 }
+
 .relief_bg {
   background-image: url("data:image/svg+xml,%3Csvg xmlns='http://www.w3.org/2000/svg' width='351' height='292.5' viewBox='0 0 1080 900'%3E%3Cg fill-opacity='0.03'%3E%3Cpolygon fill='%23444' points='90 150 0 300 180 300'/%3E%3Cpolygon points='90 150 180 0 0 0'/%3E%3Cpolygon fill='%23AAA' points='270 150 360 0 180 0'/%3E%3Cpolygon fill='%23DDD' points='450 150 360 300 540 300'/%3E%3Cpolygon fill='%23999' points='450 150 540 0 360 0'/%3E%3Cpolygon points='630 150 540 300 720 300'/%3E%3Cpolygon fill='%23DDD' points='630 150 720 0 540 0'/%3E%3Cpolygon fill='%23444' points='810 150 720 300 900 300'/%3E%3Cpolygon fill='%23FFF' points='810 150 900 0 720 0'/%3E%3Cpolygon fill='%23DDD' points='990 150 900 300 1080 300'/%3E%3Cpolygon fill='%23444' points='990 150 1080 0 900 0'/%3E%3Cpolygon fill='%23DDD' points='90 450 0 600 180 600'/%3E%3Cpolygon points='90 450 180 300 0 300'/%3E%3Cpolygon fill='%23666' points='270 450 180 600 360 600'/%3E%3Cpolygon fill='%23AAA' points='270 450 360 300 180 300'/%3E%3Cpolygon fill='%23DDD' points='450 450 360 600 540 600'/%3E%3Cpolygon fill='%23999' points='450 450 540 300 360 300'/%3E%3Cpolygon fill='%23999' points='630 450 540 600 720 600'/%3E%3Cpolygon fill='%23FFF' points='630 450 720 300 540 300'/%3E%3Cpolygon points='810 450 720 600 900 600'/%3E%3Cpolygon fill='%23DDD' points='810 450 900 300 720 300'/%3E%3Cpolygon fill='%23AAA' points='990 450 900 600 1080 600'/%3E%3Cpolygon fill='%23444' points='990 450 1080 300 900 300'/%3E%3Cpolygon fill='%23222' points='90 750 0 900 180 900'/%3E%3Cpolygon points='270 750 180 900 360 900'/%3E%3Cpolygon fill='%23DDD' points='270 750 360 600 180 600'/%3E%3Cpolygon points='450 750 540 600 360 600'/%3E%3Cpolygon points='630 750 540 900 720 900'/%3E%3Cpolygon fill='%23444' points='630 750 720 600 540 600'/%3E%3Cpolygon fill='%23AAA' points='810 750 720 900 900 900'/%3E%3Cpolygon fill='%23666' points='810 750 900 600 720 600'/%3E%3Cpolygon fill='%23999' points='990 750 900 900 1080 900'/%3E%3Cpolygon fill='%23999' points='180 0 90 150 270 150'/%3E%3Cpolygon fill='%23444' points='360 0 270 150 450 150'/%3E%3Cpolygon fill='%23FFF' points='540 0 450 150 630 150'/%3E%3Cpolygon points='900 0 810 150 990 150'/%3E%3Cpolygon fill='%23222' points='0 300 -90 450 90 450'/%3E%3Cpolygon fill='%23FFF' points='0 300 90 150 -90 150'/%3E%3Cpolygon fill='%23FFF' points='180 300 90 450 270 450'/%3E%3Cpolygon fill='%23666' points='180 300 270 150 90 150'/%3E%3Cpolygon fill='%23222' points='360 300 270 450 450 450'/%3E%3Cpolygon fill='%23FFF' points='360 300 450 150 270 150'/%3E%3Cpolygon fill='%23444' points='540 300 450 450 630 450'/%3E%3Cpolygon fill='%23222' points='540 300 630 150 450 150'/%3E%3Cpolygon fill='%23AAA' points='720 300 630 450 810 450'/%3E%3Cpolygon fill='%23666' points='720 300 810 150 630 150'/%3E%3Cpolygon fill='%23FFF' points='900 300 810 450 990 450'/%3E%3Cpolygon fill='%23999' points='900 300 990 150 810 150'/%3E%3Cpolygon points='0 600 -90 750 90 750'/%3E%3Cpolygon fill='%23666' points='0 600 90 450 -90 450'/%3E%3Cpolygon fill='%23AAA' points='180 600 90 750 270 750'/%3E%3Cpolygon fill='%23444' points='180 600 270 450 90 450'/%3E%3Cpolygon fill='%23444' points='360 600 270 750 450 750'/%3E%3Cpolygon fill='%23999' points='360 600 450 450 270 450'/%3E%3Cpolygon fill='%23666' points='540 600 630 450 450 450'/%3E%3Cpolygon fill='%23222' points='720 600 630 750 810 750'/%3E%3Cpolygon fill='%23FFF' points='900 600 810 750 990 750'/%3E%3Cpolygon fill='%23222' points='900 600 990 450 810 450'/%3E%3Cpolygon fill='%23DDD' points='0 900 90 750 -90 750'/%3E%3Cpolygon fill='%23444' points='180 900 270 750 90 750'/%3E%3Cpolygon fill='%23FFF' points='360 900 450 750 270 750'/%3E%3Cpolygon fill='%23AAA' points='540 900 630 750 450 750'/%3E%3Cpolygon fill='%23FFF' points='720 900 810 750 630 750'/%3E%3Cpolygon fill='%23222' points='900 900 990 750 810 750'/%3E%3Cpolygon fill='%23222' points='1080 300 990 450 1170 450'/%3E%3Cpolygon fill='%23FFF' points='1080 300 1170 150 990 150'/%3E%3Cpolygon points='1080 600 990 750 1170 750'/%3E%3Cpolygon fill='%23666' points='1080 600 1170 450 990 450'/%3E%3Cpolygon fill='%23DDD' points='1080 900 1170 750 990 750'/%3E%3C/g%3E%3C/svg%3E");
-  }
+}
 </style>
-
--- src/config/extresources.ts
+++ src/config/extresources.ts
@@ -1,9 +1,14 @@
-interface ExtResources {
-  name: string
-  link: string
+interface Resource {
+  name: string;
+  link: string;
 }
 
-export const ext_resources: ExtResources[] = [
+export const ext_resources: Resource[] = [
   { name: "pasta", link: "https://pasta.example.com" },
-  {name: "storage", link: "https://storage.example.com"},
+  { name: "storage", link: "https://storage.example.com" },
+];
+
+export const in_resources: Resource[] = [
+  { name: "Мои курсы", link: "https://localhost:6379/courses" },
+  { name: "Магазин", link: "https://localhost:6379/shop" },
 ];
