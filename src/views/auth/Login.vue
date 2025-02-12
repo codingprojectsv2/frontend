59d21c8b96026bbfb2bfd40838021a71d5fef458
stenli <s.nevrovsky@ya.ru>
2025-02-12 11:12:32 +0300

    creating navbar

---
+++ src/views/auth/Login.vue
@@ -0,0 +1,11 @@
+<template>
+<div class="min-h-screen dark:bg-gray-900">
+    <Navbar />
+    
+    <div class="flex flex-col h-full">
+        <div class="bg-gray-700 rounded-lg">
+            <h4>Login</h4>
+        </div>
+    </div>
+  </div>
+</template>
